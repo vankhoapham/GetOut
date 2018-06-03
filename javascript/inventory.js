d86ab8d6e9c0f81850d535e46b06799691cf27c9
AbyssOfMisery <39228340+AbyssOfMisery@users.noreply.github.com>
2018-06-02 21:45:21 -0400

    nothing

--- javascript/inventory.js
+++ javascript/inventory.js
@@ -4,9 +4,9 @@ canvas1.width = 50;
 canvas1.height = 640;
 
 bgImg = new Image();
-bgImg.src = "images/Dungeon_A1.png";
+bgImg.src = "images/box.png";
 const SIZE = 64;
-var mapX=0; mapY=33;
+var mapX=0; mapY=0;
 var map = [
     [0],
     [0],
@@ -18,31 +18,19 @@ var map = [
     [0],
     [0],
     [0],
+
 ];
+
 bgImg.onload=function(){
     drawInventory();
 };
 window.addEventListener('keydown', pickUp, false);
-function pickUp(event) {
-
-        if (event.keyCode == 74)// key J
-        {
-            map.splice(0,1,"1");
-        }console.log(map[0][0]);
-
-    }
 
 
 function drawInventory(){
     for(var row = 0; row < map.length;row++){
         Y = row * SIZE;
         for (var col = 0; col < map[row].length;col++){
-            if(map[row][col]===1){
-                mapX = 50;
-            }
-            else if(map[row][col]===0){
-                mapX = 0;
-            }
             X = col * SIZE;
             ctx.drawImage(bgImg,
                 mapX,mapY,SIZE,SIZE,
@@ -52,3 +40,19 @@ function drawInventory(){
 
 
 }
+
+var item = [[0]];
+itemImg = new Image();
+itemImg.src = "images/item/1.png";
+
+function pickUp(event) {
+
+    if (event.keyCode == 74)// key J
+    {
+        item.push(1);
+    }console.log(item);
+}
+
+itemImg.onload=function(){
+    drawItem();
+};
